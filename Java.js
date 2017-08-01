6942a39b5648920a50e5a58cb45900ab46e54d07
EgideChichi <egidechichi15@gmail.com>
2017-08-01 15:21:22 -0400

    Updating

--- Java.js
+++ Java.js
@@ -9,3 +9,24 @@ if (bulletstyle=='line-through')
  document.getElementById(Boom).style.textDecoration='line-through';
 
 }
+//function egide()
+//{
+//names =document.getElementsByClassName('task');
+ //var i = 0;
+ //while (i < names.length )
+//{
+//names[i].style.textDecoration='none';
+//i++;
+// names[y].getElementById('task').style.textDecoration='none';
+//}
+//}
+
+function egide()
+{
+names =document.getElementsByClassName('task');
+for (y=0 ; y < names.length ; y++)
+{
+names[y].style.textDecoration='none';
+// names[y].getElementById('task').style.textDecoration='none';
+}
+}
